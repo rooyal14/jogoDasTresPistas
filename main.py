272c47158b9852cea676b17e1913adb0ee1e35dc
Rodrigo <zwi.rodrigo@gmail.com>
2023-09-29 04:10:11 -0400

    Banco de perguntas registrado

--- main.py
+++ main.py
@@ -18,7 +18,15 @@ O sistema deve prever um placar ao finalizar as respostas. Exemplo. Perguntar em
 
 class Perguntas:
     perguntas = [["Bioma ", "Mata Atlântica", "Sociedade", "Ciclo da Água", "Ciclo do Fósforo", "Inversão Térmica", "Pegada de Carbono", "Efeito Estufa", "Eutrofização da água", "Decompositores"],
-    ["organismos da fauna e da flora interagindo entre si", "Vegetação densa e perene", "interação entre indivíduos", "Precipitação", "Erosão e formação do solo", "Assimilação", "", "", "", ""],
-    ["comunidades biológicas", "Bioma mais degradado", "", "", "", "", "", "", "", "", ""],["conjunto de diferentes ecossistemas", "Vegetação com ou sem Araucária", "", "", "", "", "", "", "", ""]]
+    ["Organismos da fauna e da flora interagindo entre si", "Vegetação densa e perene", "Interação entre indivíduos", "Precipitação", "Erosão e formação do solo", "Poluição não se dissipa", "Gerado ao longo do ciclo de vida de um produto", "Retenção de Raios Solares", "Excesso de nutrientes na água", "Podem atuar em vários níveis tróficos"],
+    ["Comunidades biológicas", "Bioma mais degradado", "Mesma espécie", "Evaporação", "Assimilação", "Ocorre em dias frios", "Contém as emissões totais (diretas e indiretas)", "Tem relação com combustíveis fósseis", "Fitoplâncton se prolifera absurdamente rápido", "Reciclam Matéria Orgânica"],
+    ["Conjunto de diferentes ecossistemas", "Vegetação com ou sem Araucária", "Organizados em castas ", "Condensação", "Identificado como P na tabela periódica.", "Cortina de fuligem impede a passagem de raios solares", "Gases de Efeito Estufa", "Concentração de gases poluentes na Atmosfera", "Bactérias consomem o O2 causando a morte de animais e protozoários (peixes e zooplâncton)", "Devolvem nutrientes aos produtores, que estão no primeiro nível trófico"]]
 
-print(Perguntas().perguntas[1][0])
+for i in range(10):
+    bd = Perguntas()
+    print("Pergunta Nº"+str(i+1))
+    print(bd.perguntas[0][i])
+    print(bd.perguntas[1][i])
+    print(bd.perguntas[2][i])
+    print(bd.perguntas[3][i])
+    print()
