06823cad80169eb512491d35110842f63ec32fc9
alemão <zwiro@LAPTOP-ICMUKP2K>
2023-09-29 21:11:52 -0400

    Classificação individual de perguntas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random as r
 """
 Dividiremos a sala em três equipes, duas vão duelar e uma será a plateia(professor).
 
@@ -16,17 +17,51 @@ Quando vai para a plateia: 10 pontos
 O sistema deve prever um placar ao finalizar as respostas. Exemplo. Perguntar em qual pista a questão foi solucionada e qual o time responsável.
 """
 
-class Perguntas:
-    perguntas = [["Bioma ", "Mata Atlântica", "Sociedade", "Ciclo da Água", "Ciclo do Fósforo", "Inversão Térmica", "Pegada de Carbono", "Efeito Estufa", "Eutrofização da água", "Decompositores"],
-    ["Organismos da fauna e da flora interagindo entre si", "Vegetação densa e perene", "Interação entre indivíduos", "Precipitação", "Erosão e formação do solo", "Poluição não se dissipa", "Gerado ao longo do ciclo de vida de um produto", "Retenção de Raios Solares", "Excesso de nutrientes na água", "Podem atuar em vários níveis tróficos"],
-    ["Comunidades biológicas", "Bioma mais degradado", "Mesma espécie", "Evaporação", "Assimilação", "Ocorre em dias frios", "Contém as emissões totais (diretas e indiretas)", "Tem relação com combustíveis fósseis", "Fitoplâncton se prolifera absurdamente rápido", "Reciclam Matéria Orgânica"],
-    ["Conjunto de diferentes ecossistemas", "Vegetação com ou sem Araucária", "Organizados em castas ", "Condensação", "Identificado como P na tabela periódica.", "Cortina de fuligem impede a passagem de raios solares", "Gases de Efeito Estufa", "Concentração de gases poluentes na Atmosfera", "Bactérias consomem o O2 causando a morte de animais e protozoários (peixes e zooplâncton)", "Devolvem nutrientes aos produtores, que estão no primeiro nível trófico"]]
 
-for i in range(10):
-    bd = Perguntas()
-    print("Pergunta Nº"+str(i+1))
-    print(bd.perguntas[0][i])
-    print(bd.perguntas[1][i])
-    print(bd.perguntas[2][i])
-    print(bd.perguntas[3][i])
-    print()
+perguntas = [["Bioma ", "Mata Atlântica", "Sociedade", "Ciclo da Água", "Ciclo do Fósforo", "Inversão Térmica", "Pegada de Carbono", "Efeito Estufa", "Eutrofização da água", "Decompositores"],
+["Organismos da fauna e da flora interagindo entre si", "Vegetação densa e perene", "Interação entre indivíduos", "Precipitação", "Erosão e formação do solo", "Poluição não se dissipa", "Gerado ao longo do ciclo de vida de um produto", "Retenção de Raios Solares", "Excesso de nutrientes na água", "Podem atuar em vários níveis tróficos"],
+["Comunidades biológicas", "Bioma mais degradado", "Mesma espécie", "Evaporação", "Assimilação", "Ocorre em dias frios", "Contém as emissões totais (diretas e indiretas)", "Tem relação com combustíveis fósseis", "Fitoplâncton se prolifera absurdamente rápido", "Reciclam Matéria Orgânica"],
+["Conjunto de diferentes ecossistemas", "Vegetação com ou sem Araucária", "Organizados em castas ", "Condensação", "Identificado como P na tabela periódica.", "Cortina de fuligem impede a passagem de raios solares", "Gases de Efeito Estufa", "Concentração de gases poluentes na Atmosfera", "Bactérias consomem o O2 causando a morte de animais e protozoários (peixes e zooplâncton)", "Devolvem nutrientes aos produtores, que estão no primeiro nível trófico"]]
+nPerguntas = len(perguntas[0])
+"""
+class PerguntasController:
+    def __init__(self, questionsArray):
+        self.perguntasDisponiveis = self.randomizeQuestions(questionsArray)
+    
+    def randomizeQuestions(questionsArray):
+        perguntasDisponiveis = []
+        for i in range(nPerguntas, 0):
+            selectedIndex = r.randint(0, i)
+            selectedQuestion = Pergunta(questionsArray[0].pop(selectedIndex),
+                                        questionsArray[1].pop(selectedIndex),
+                                        questionsArray[2].pop(selectedIndex),
+                                        questionsArray[3].pop(selectedIndex))
+            perguntasDisponiveis.append(selectedQuestion)
+        return perguntasDisponiveis
+    
+    def printAllQuestions(self):
+        for i in range(10):
+            bd = self.perguntasDisponiveis
+            print("Pergunta Nº"+str(i+1))
+            print(bd[0][i])
+            print(bd[1][i])
+            print(bd[2][i])
+            print(bd[3][i])
+            print()
+"""
+
+class Pergunta:
+    def __init__(self, nome, primeiraPista, segundaPista, terceiraPista):
+        self.nome = nome
+        self.primeiraPista = primeiraPista
+        self.segundaPista = segundaPista
+        self.terceiraPista = terceiraPista
+
+arrayPerguntasIndividuais = []
+for i in range(nPerguntas):
+    selectedQuestion = Pergunta(perguntas[0][i],
+                                perguntas[1][i],
+                                perguntas[2][i],
+                                perguntas[3][i])
+    arrayPerguntasIndividuais.append(selectedQuestion)
+print()
